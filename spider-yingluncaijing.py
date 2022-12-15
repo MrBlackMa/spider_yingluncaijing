44a7e0b955e01a33a33925eeef8ab8c4d2fa1284
malixi <malixi@co-mall.com>
2022-12-15 20:10:10 +0800

    学习，爬取英伦财经的代码demo

--- spider-yingluncaijing.py
+++ spider-yingluncaijing.py
@@ -3,17 +3,16 @@ from os import makedirs
 from os.path import exists
 import requests
 import logging
-import re
 from urllib.parse import urljoin
 import cloudscraper
 import multiprocessing
-from lxml import  etree
+from lxml import etree
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
 BASE_URL = 'https://cn.investing.com/news/cryptocurrency-news'
-PRO_BASE_URL='https://cn.investing.com/'
+PRO_BASE_URL = 'https://cn.investing.com/'
 TOTAL_PAGE = 10
 
 RESULTS_DIR = 'results'
@@ -23,7 +22,7 @@ exists(RESULTS_DIR) or makedirs(RESULTS_DIR)
 def scrape_page(url):
     try:
         # 获取页面的内容
-       # response = requests.get(url)
+        # response = requests.get(url)
         # 制裁Cloudflare的反爬虫机制制裁
         scraper = cloudscraper.create_scraper(browser={'browser': 'chrome', 'platform': 'windows', 'mobile': False})
         response = scraper.get(url)
@@ -51,19 +50,18 @@ def parse_index(html):
     # result = h.xpath('//section[@id="leftColumn"]//div[@class="textDiv"]//a[@class="title"]//text()')
     # #print(result)
     # 获取超链接里面的内容 获取内容详情
-    detail_urls=h.xpath('//section[@id="leftColumn"]//div[@class="textDiv"]//a//@href')
+    detail_urls = h.xpath('//section[@id="leftColumn"]//div[@class="textDiv"]//a//@href')
     print(type(detail_urls))
     score = []
     # 取出包含有comments的内容
     for item in detail_urls:
-       result = "comments" not in item
-       if result:
-           score.append(item)
+        comments_a = "comments" not in item
+        if comments_a:
+            score.append(item)
     for url in score:
         detail_url = urljoin(PRO_BASE_URL, url)
         logging.info('get detail url %s', detail_url)
-        yield detail_url # 返回一个经过处理的detail_url 集合
-
+        yield detail_url  # 返回一个经过处理的detail_url 集合
 
     # for item in detail_urls:
     #     # # 如果
@@ -73,24 +71,21 @@ def parse_index(html):
     #     detail_url = urljoin(PRO_BASE_URL, item)
     #     logging.info('get detail url %s', detail_url)
     #     yield detail_url
-       # 分割字符串
-       # last_url = ""
-       # print(detail_url)
-       # last_url+=detail_url.split('/')[3]
-       # print(last_url)
-
-
+    # 分割字符串
+    # last_url = ""
+    # print(detail_url)
+    # last_url+=detail_url.split('/')[3]
+    # print(last_url)
 
 
 def scrape_detail(url):
     """
     scrape detail page and return its html
+    :param url:
     :param page: page of detail page
     :return: html of detail page
     """
     return scrape_page(url)
-
-
 
 
 def save_data(data):
@@ -99,7 +94,8 @@ def save_data(data):
     :param data:
     :return:
     """
-    name = data.get('name')
+    name = data.get('title')
+    print(name)
     data_path = f'{RESULTS_DIR}/{name}.json'
     json.dump(data, open(data_path, 'w', encoding='utf-8'),
               ensure_ascii=False, indent=2)
@@ -109,19 +105,28 @@ def save_data(data):
 def parse_detail(detail_html):
     h = etree.HTML(detail_html)
     # 获取文本标题
-    get_title=h.xpath('//h1[@class="articleHeader"]/text()')
-    print(get_title)
+    get_title = h.xpath('//h1[@class="articleHeader"]/text()')
+
+    #获取时间
+    get_time = h.xpath('//div[@class="contentSectionDetails"]/span/text()')
+
     # contains 表示属性多值选择器
     # 获取文本内容
-    result=h.xpath('//div[contains(@class,"WYSIWYG")]/p//text()')
-    print(result)
+    content_text = h.xpath('//div[contains(@class,"WYSIWYG")]/p//text()')
+
+    return {
+        'title': get_title,
+        'time' : get_time,
+        'text' : content_text
+    }
+
 
 def main():
     # 获取主页的内容
-    for page in range(1,1+1):
+    for page in range(1, 1 + 1):
         index_html = scrape_index(page)
-    # 解析主页的内容 获取到主页列表的所有详情页
-        detail_urls=parse_index(index_html)
+        # 解析主页的内容 获取到主页列表的所有详情页
+        detail_urls = parse_index(index_html)
     for detail_url in detail_urls:
         # 解析单个详情页内容
         detail_html = scrape_detail(detail_url)
@@ -129,8 +134,9 @@ def main():
         # logging.info('get detail data %s', data)
         # logging.info('saving data to json file')
         # 保存内容
-    #     save_data(data)
-    #     logging.info('data saved successfully')
+        print(data)
+        save_data(data)
+        logging.info('data saved successfully')
 
 
 # if __name__ == '__main__':
